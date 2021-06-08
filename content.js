70248729415e834e63298d5d2276e548218fef7e
makarworld <58076271+makarworld@users.noreply.github.com>
2021-06-08 22:07:02 +0300

    Change wait to 250 ms

--- content.js
+++ content.js
@@ -6,17 +6,19 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
+var wait_time = 250 // milliseconds
+
 console.log("AutoApprove started!")
 
 window.onload = async function() {
-    console.log("Страница прогрузилась!")
+    console.log("Page loaded!")
     // click to box
     var box = document.querySelector('input[data-indeterminate="false"]')
     if (box != null) {
         document.querySelector('input[data-indeterminate="false"]').click();
         console.log("click on box!");
     }
-    while (document.querySelector('button[class="button button-secondary button-large text-1-5rem text-bold mx-1"]') == null) {await sleep(3000); console.log("Жду капчу...")}
+    while (document.querySelector('button[class="button button-secondary button-large text-1-5rem text-bold mx-1"]') == null) {await sleep(wait_time); console.log("Wait button...")}
     // set interval to click approve
-    clck_approve = setInterval(() => { document.querySelector('button[class="button button-secondary button-large text-1-5rem text-bold mx-1"]').click(); console.log("Try to click on Approve"); }, 3000)
+    clck_approve = setInterval(() => { document.querySelector('button[class="button button-secondary button-large text-1-5rem text-bold mx-1"]').click(); console.log("Try to click on Approve"); }, wait_time)
 }
